d6306b6d6c9df28ca91d9fa989015d38578d438f
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-24 01:24:29 +0900

    json

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import { Hono } from 'hono'
-import { renderer } from './renderer'
+import { Hono } from 'hono';
+import { renderer } from './renderer';
 
-const app = new Hono()
+import jsonFormatter from './json-formatter';
 
-app.get('*', renderer)
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
-})
+app.get('*', renderer);
 
-export default app
+app.mount("/json", jsonFormatter);
+
+export default app;
---
+++ src/json-formatter/index.tsx
@@ -0,0 +1,16 @@
+import { Hono } from 'hono';
+import { renderer } from "/renderer.tsx";
+
+const app = new Hono();
+
+app.get('*', renderer);
+
+app.get('/format', (c) => {
+  const body = await c.req.parseBody();
+  return c.render(<>
+    <code>{JSON.parse(body.json)}</code>
+    <a href="/json">別の</a>
+  </>);
+});
+
+export default app;
